55acfdad4bc7fc466aa1d3df0746092aea84c7fe
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2023-08-13 20:58:18 -0300

    Inicio projeto, primeiro painel

---
+++ src/view/acpdv.view.principal.pas
@@ -0,0 +1,27 @@
+unit acpdv.view.principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    pnlContainer: TPanel;
+    pnlTitle: TPanel;
+    Panel2: TPanel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
